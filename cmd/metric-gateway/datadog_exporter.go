ccc2aab5f4e17a08343f038182af70790075e52b
Fernando Alonso <krakatoa1987@gmail.com>
2017-01-03 15:43:38 -0300

    Added ticker on DatadogExporter

--- cmd/metric-gateway/datadog_exporter.go
+++ cmd/metric-gateway/datadog_exporter.go
@@ -7,20 +7,24 @@ import (
   "encoding/gob"
   "bytes"
   "encoding/base64"
+
+  "time"
   //"github.com/zorkian/go-datadog-api"
 )
 
 type DatadogExporter struct {
+  ticker        *time.Ticker
   queue         *boltq.BoltQ
 }
 
 func NewDatadogExporter() *DatadogExporter {
-  q, err := boltq.NewBoltQ("datadog.queue", 2000, boltq.POP_ON_FULL)
+  q, err := boltq.NewBoltQ("datadog.queue", 3000, boltq.POP_ON_FULL)
   if err != nil {
     panic("error opening bolq queue")
   }
 
   return &DatadogExporter{
+    ticker:       time.NewTicker(time.Duration(1000) * time.Millisecond),
     queue:        q,
   }
 }
@@ -40,27 +44,34 @@ func (d *DatadogExporter) Write(metric BaseMetric) {
     base64.StdEncoding.Encode(base64Text, bin.Bytes())
     err = d.queue.Enqueue(base64Text)
     log.Printf("Written: %v", base64Text)
+
+    log.Printf("QUEUE SIZE: %d", d.queue.Size())
   }
 }
 
 func (d *DatadogExporter) Consume() {
-  for {
-    value, _ := d.queue.Dequeue()
-    if value != nil {
-      base64Text := make([]byte, base64.StdEncoding.DecodedLen(len(value)))
-      base64.StdEncoding.Decode(base64Text, value)
+  for _ = range d.ticker.C {
+    for i := 1; i <= 30; i++ {
+      value, _ := d.queue.Dequeue()
+      if value != nil {
+        base64Text := make([]byte, base64.StdEncoding.DecodedLen(len(value)))
+        base64.StdEncoding.Decode(base64Text, value)
 
-      var metric BaseMetric
-      var binReader *bytes.Reader = bytes.NewReader(base64Text)
-      dec := gob.NewDecoder(binReader)
+        var metric BaseMetric
+        var binReader *bytes.Reader = bytes.NewReader(base64Text)
+        dec := gob.NewDecoder(binReader)
 
-      err := dec.Decode(&metric)
-      if err != nil {
-        log.Fatal("decode error:", err)
+        err := dec.Decode(&metric)
+        if err != nil {
+          log.Fatal("decode error:", err)
+        } else {
+          d.exportMetric(metric)
+        }
       } else {
-        d.exportMetric(metric)
+        break
       }
     }
+    log.Printf("QUEUE SIZE: %d", d.queue.Size())
   }
 }
 
